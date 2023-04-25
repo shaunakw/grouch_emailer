5d51b144c7c05c533d21eccc3f356776d59b1a55
Shaunak Warty <shaunak.warty@gmail.com>
2023-04-25 18:18:45 -0400

    error handle

--- main.py
+++ main.py
@@ -10,43 +10,46 @@ print('Loaded all notifiers')
 print()
 
 while True:
-  for n in notifiers:
-    prev_spots = n.spots
-    prev_wl_spots = n.wl_spots
-    n.fetch_info()
+  try:
+    for n in notifiers:
+      prev_spots = n.spots
+      prev_wl_spots = n.wl_spots
+      n.fetch_info()
+      
+      if n.spots > 0 and not prev_spots > 0:
+        contents = [
+          'The following course is now open to register:',
+          f'Name: {n.course.name}',
+          f'CRN: {n.course.crn}',
+          f'Spots open: {n.spots}'
+        ]
+        notify(n, contents)
+      elif not n.spots > 0 and prev_spots > 0:
+        contents = [
+          'The following course is now closed to register:',
+          f'Name: {n.course.name}',
+          f'CRN: {n.course.crn}'
+        ]
+        notify(n, contents)
+      elif n.wl_spots > 0 and not prev_wl_spots > 0:
+        contents = [
+          'The following course is now open to waitlist:',
+          f'Name: {n.course.name}',
+          f'CRN: {n.course.crn}',
+          f'Waitlist spots open: {n.wl_spots}'
+        ]
+        notify(n, contents)
+      elif not n.wl_spots > 0 and prev_wl_spots > 0:
+        contents = [
+          'The following course is now closed to waitlist:',
+          f'Name: {n.course.name}',
+          f'CRN: {n.course.crn}'
+        ]
+        notify(n, contents)
     
-    if n.spots > 0 and not prev_spots > 0:
-      contents = [
-        'The following course is now open to register:',
-        f'Name: {n.course.name}',
-        f'CRN: {n.course.crn}',
-        f'Spots open: {n.spots}'
-      ]
-      notify(n, contents)
-    elif not n.spots > 0 and prev_spots > 0:
-      contents = [
-        'The following course is now closed to register:',
-        f'Name: {n.course.name}',
-        f'CRN: {n.course.crn}'
-      ]
-      notify(n, contents)
-    elif n.wl_spots > 0 and not prev_wl_spots > 0:
-      contents = [
-        'The following course is now open to waitlist:',
-        f'Name: {n.course.name}',
-        f'CRN: {n.course.crn}',
-        f'Waitlist spots open: {n.wl_spots}'
-      ]
-      notify(n, contents)
-    elif not n.wl_spots > 0 and prev_wl_spots > 0:
-      contents = [
-        'The following course is now closed to waitlist:',
-        f'Name: {n.course.name}',
-        f'CRN: {n.course.crn}'
-      ]
-      notify(n, contents)
-  
-  now = datetime.now()
-  sys.stdout.write(f'\rLast time checked: {now.strftime("%I:%M:%S%p")}')
-  
-  time.sleep(1)
+    now = datetime.now()
+    sys.stdout.write(f'\rLast time checked: {now.strftime("%I:%M:%S%p")}')
+    
+    time.sleep(1)
+  except:
+    pass
